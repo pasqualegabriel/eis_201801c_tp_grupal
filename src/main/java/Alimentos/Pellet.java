9030d0f4c656c60f5317f0008b77e8685980718c
f.autalan <n.autalan@gmail.com>
2018-05-16 13:46:53 -0300

    pacman a la hora de comer un pellet debilita a los fantasmas

---
+++ src/main/java/Alimentos/Pellet.java
@@ -0,0 +1,9 @@
+package Alimentos;
+
+public class Pellet extends Alimento {
+    public Pellet(int unValor) {
+        super(unValor);
+    }
+    @Override
+    public boolean isPellet(){return true; }
+}
